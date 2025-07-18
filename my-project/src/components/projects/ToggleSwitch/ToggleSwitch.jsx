c6bbb104bad32147ad82ae0c3742cf4f0be49b32
Pallavi Kumari <paalavikr028@gmail.com>
2025-07-18 19:19:05 +0530

    new code

--- my-project/src/components/projects/ToggleSwitch/ToggleSwitch.jsx
+++ my-project/src/components/projects/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./ToggleSwitch.css";
+import { IoIosSwitch } from "react-icons/io";
 export const ToggleSwitch = () => {
     const [isOn, setIsOn] = useState(false);
 
@@ -10,10 +11,13 @@ export const ToggleSwitch = () => {
     const checkIson = isOn ? "on" : "off";
     const toggleBGColor = { backgroundColor : isOn ? "#4caf50" : "" };
     return (
+        <>
+        <h1 style={{color: "#000", textAlign: "center"}}>Toggle Switch <IoIosSwitch /> </h1>
         <div className="toggle-switch" style={toggleBGColor} onClick={handleToggleSwitch}>
             <div className={`switch ${checkIson}`}>
                 <span className="switch-state">{checkIson}</span>
             </div>
         </div>
+        </>
     );
 };
